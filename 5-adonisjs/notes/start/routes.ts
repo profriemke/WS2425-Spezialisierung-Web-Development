2ca397aac316136e72a8569636168b274c7b87ed
prof_riemke <2960904+profriemke@users.noreply.github.com>
2024-12-05 15:59:00 +0100

    05.12

--- 5-adonisjs/notes/start/routes.ts
+++ 5-adonisjs/notes/start/routes.ts
@@ -9,4 +9,13 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+
+const notes = [
+    {id: 1, text: 'erste Notiz'},
+    {id:2, text: 'zweite Notiz'}
+]
+
+//router.on('/').render('pages/home', { notes} )
+router.get('/', async ({view })=>{
+    return view.render('pages/home', {  notes })
+})
